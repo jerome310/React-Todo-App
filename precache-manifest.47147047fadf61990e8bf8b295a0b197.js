f4088c428a0cba530e3896e218bbd0733064f728
Jerome Young <dreammedia365@yahoo.com>
2020-08-28 10:58:59 -0700

    Updates

---
+++ precache-manifest.47147047fadf61990e8bf8b295a0b197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1208691c0333a704987d647596566720",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "b967e12696367afda179",
+    "url": "/React-Todo-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3702adc225b57a1bf053",
+    "url": "/React-Todo-App/static/js/2.4cec0e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.4cec0e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b967e12696367afda179",
+    "url": "/React-Todo-App/static/js/main.d2aef557.chunk.js"
+  },
+  {
+    "revision": "35169203a1dc3ace5fd5",
+    "url": "/React-Todo-App/static/js/runtime-main.8f6abf7e.js"
+  }
+]);
